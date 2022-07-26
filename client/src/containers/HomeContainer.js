146f341308a5b25e5d9a201407e18326cb31efaf
Cetins <69481095+Cetins@users.noreply.github.com>
2022-07-26 21:45:26 +0100

    services service file added, tested in insomnia

--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -2,13 +2,19 @@ import React, { useEffect, useState } from "react";
 import Products from "../components/Products";
 import MainTitle from "../components/MainTitle";
 import ProductService from "../services/ProductService";
+import ServiceService from "../services/ServiceService";
 
 const HomeContainer = () => {
     const [products, setProducts] = useState([]);
+    const [services, setServices] = useState([]);
 
     useEffect(() => {
         ProductService.getProducts()
             .then(products => setProducts(products));
+    }, []);
+    useEffect(() => {
+        ServiceService.getServices()
+            .then(services => setServices(services));
     }, []);
 
     return (
