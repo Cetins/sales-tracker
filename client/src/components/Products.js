a73f230f81d3108cebb58387e82a75b6949c0f5e
Cetins <69481095+Cetins@users.noreply.github.com>
2022-07-26 16:12:07 +0100

    products table styled

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -3,10 +3,14 @@ import SubTitle from "./SubTitle"
 import ProductsTable from "./ProductsTable"
 
 const Products = ({ products }) => {
+    const stockTotal = products.reduce((total, sale) => total + sale.price, 0);
+    const stockTitle = `Net Stock Worth is £ ${stockTotal}`
+
     return (
         <div className="parent-container">
             <SubTitle title="Products"/>
-            <ProductsTable products={products}/>
+            <ProductsTable products={products} stockTotal={stockTotal}/>
+            <SubTitle title={stockTitle}/>
         </div>
     )
 }
--- client/src/components/ProductsTable.js
+++ client/src/components/ProductsTable.js
@@ -1,7 +1,9 @@
 import React from "react";
+import SubTitle from "./SubTitle";
+import "../styles/Table.css"
 
 const ProductsTable = ({ products }) => {
-    const listItems = products.map(item => {
+    const tableItems = products.map(item => {
         return (
             <tr key={item._id}>
                 <td>{item.title}</td>
@@ -13,7 +15,22 @@ const ProductsTable = ({ products }) => {
         )
     });
 
-    return
+    return (
+            <table className="styled-table">
+                <thead>
+                    <tr>
+                        <th>Product</th>
+                        <th>Manufacturer</th>
+                        <th>Price</th>
+                        <th>Sale Price</th>
+                        <th>Stock</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableItems}
+                </tbody>
+            </table>
+    )
 }
 
 export default ProductsTable;
