edc67844d83b32ef2903412ddccbc3aeb33de2d9
Cetins <69481095+Cetins@users.noreply.github.com>
2022-07-26 14:42:43 +0100

    title components added

---
+++ client/src/components/MainTitle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainTitle = ({ title }) => {
+    return <h1 className="main-title">{ title }</h1>
+}
+
+export default MainTitle;
---
+++ client/src/components/SubTitle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SubTitle = ({ title }) => {
+    return <h2 className="sub-title">{ title }</h2>
+}
+
+export default SubTitle;
