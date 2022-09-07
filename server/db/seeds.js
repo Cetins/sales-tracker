3c34bb9560244e8e6b2ff9ae49f3df79199ef2dd
Cetins <69481095+Cetins@users.noreply.github.com>
2022-09-07 21:47:18 +0100

    seeds db drop deactivated, data updated

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,74 +1,74 @@
 use sale_tracker;
-db.dropDatabase();
+// db.dropDatabase();
 
 db.shop.insertMany([
     {
-        name: "My Shop Ltd",
-        email: "myshop@mail.com"
+        name: "Brush Barbers",
+        email: "brushbarbers@mail.com"
     }
 ])
 
 db.products.insertMany([
     {
-        title: "test product1",
+        title: "Sea Salt Spray",
         manufacturer: "company ltd",
-        price: 10,
-        cost: 10,
+        price: 18,
+        cost: 9,
         stock: 23
     },
     {
-        title: "test product2",
+        title: "Hair Pomade",
         manufacturer: "company ltd",
-        price: 10,
-        cost: 10,
+        price: 12,
+        cost: 4.8,
         stock: 9
     },
     {
-        title: "test product3",
+        title: "Clay",
         manufacturer: "company ltd",
         price: 12,
-        cost: 10,
+        cost: 5,
         stock: 8
     },
     {
-        title: "test product4",
+        title: "Wax",
         manufacturer: "company ltd",
         price: 15,
         cost: 10,
         stock: 3
     },
     {
-        title: "test product5",
+        title: "Beard Oil",
         manufacturer: "company ltd",
         price: 15,
-        cost: 10,
+        cost: 7,
         stock: 3
     }
 ])
 
 db.services.insertMany([
     {
-        title: "test service1",
-        duration: 20,
-        price: 15
+        title: "Cut & Style",
+        duration: 30,
+        price: 35
     },
     {
-        title: "test service2",
+        title: "Beard Trim",
         duration: 20,
         price: 20
     },
     {
-        title: "test service3",
-        duration: 30,
+        title: "Kid's Haircut",
+        duration: 20,
         price: 25
     },
     {
-        title: "test service4",
+        title: "Hot Towel Shave",
         duration: 45,
-        price: 35
+        price: 32
     },
     {
-        title: "test service5",
+        title: "Vip Pampering Package",
         duration: 90,
         price: 70
     },
@@ -76,36 +76,36 @@ db.services.insertMany([
 
 db.staff.insertMany([
     {
-        name: "staff 1",
+        name: "Husso",
         sales: 0,
         service_commission: 0,
-        product_commission: 10,
+        product_commission: 0,
     },
     {
-        name: "staff 2",
+        name: "Tracey",
         sales: 0,
-        service_commission: 10,
-        product_commission: 50,
+        service_commission: 50,
+        product_commission: 5,
     }
 ])
 
 db.sales.insertMany([
-    {
-        category: "service",
-        title: "test service",
-        service_id: 121212,
-        price: 30,
-        staff: "test staff",
-        staffId: 1234,
-        date: "01/01/2022"
-    },
-    {
-        category: "product",
-        title: "test product",
-        product_id: 01234567,
-        price: 5,
-        staff: "test staff",
-        staffId: 1234,
-        date: "01/01/2022"
-    }
+    // {
+    //     category: "service",
+    //     title: "test service",
+    //     service_id: 121212,
+    //     price: 30,
+    //     staff: "test staff",
+    //     staffId: 1234,
+    //     date: "01/01/2022"
+    // },
+    // {
+    //     category: "product",
+    //     title: "test product",
+    //     product_id: 01234567,
+    //     price: 5,
+    //     staff: "test staff",
+    //     staffId: 1234,
+    //     date: "01/01/2022"
+    // }
 ])
